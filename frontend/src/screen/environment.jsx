3f80c9bcd0b740d49b72c2bc91f173d67c3ded07
satyam singh <satyam884060singh@gmail.com>
2025-05-17 14:58:42 +0530

    re-initlaized

--- frontend/src/screen/environment.jsx
+++ frontend/src/screen/environment.jsx
@@ -2,7 +2,7 @@ let IS_PROD = false;
 const server = IS_PROD ?
     "https://ai-agent-2-1.onrender.com" :
 
-    "https://your-app-name.onrender.com"
+    "https://ai-agent-2-1.onrender.com"
 
 
 export default server;
