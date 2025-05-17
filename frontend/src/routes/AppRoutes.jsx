976b6f7a793ee200d4d3d670a16e8bd4818f5d14
satyam singh <satyam884060singh@gmail.com>
2025-05-18 00:29:33 +0530

    re-initialized

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -11,10 +11,10 @@ const AppRoutes = () => {
         <BrowserRouter>
 
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/" element={<UserAuth><Home /></UserAuth>} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
-                <Route path="/project" element={<Project />} />
+                <Route path="/project" element={<UserAuth><Project /></UserAuth>} />
             </Routes>
 
         </BrowserRouter>
