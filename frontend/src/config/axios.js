c19f09957f67b8dbfceeaf430bbad1142f331a8f
satyam singh <satyam884060singh@gmail.com>
2026-01-03 23:11:23 +0530

    re-initial

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -4,7 +4,8 @@ const axiosInstance = axios.create({
     baseURL:import.meta.env.VITE_API_URL,
     headers:{
         Authorization:`Bearer ${localStorage.getItem('token')}`
-    }
+    },
+    withCredentials:true
 })
 
 export default axiosInstance;
